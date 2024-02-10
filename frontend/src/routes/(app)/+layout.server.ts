1754f8d449a54b3d446fc76f49be4adea84aba54
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-02-10 11:36:14 +0700

    feat: Logout bug

--- frontend/src/routes/(app)/+layout.server.ts
+++ frontend/src/routes/(app)/+layout.server.ts
@@ -1,8 +1,20 @@
-import type {PageServerLoad} from "../../../.svelte-kit/types/src/routes/(app)/$types";
+import type {Actions, Cookies} from "@sveltejs/kit";
+import {redirect} from "@sveltejs/kit";
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load = async ({ locals }: {locals: App.Locals}) => {
     console.log(locals.user)
     return {
         user: locals.user
+    }
+}
+
+export const actions: Actions = {
+    logout: async ({ cookies, locals }: {cookies: Cookies, locals: App.Locals}) => {
+        locals.user = null
+        cookies.delete("slcatering-token", { path: "/" })
+        cookies.delete("slcatering-name", { path: "/" })
+        cookies.delete("slcatering-username", { path: "/" })
+
+        throw redirect(303, '/')
     }
 }
