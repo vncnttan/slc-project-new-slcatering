d5c7abb6d2146a3a06e1dd38561f5f3dba397b75
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-02-13 13:11:59 +0700

    feat: Schedule Catering done

--- frontend/src/components/merchant/CreateMenuGeneralForm.svelte
+++ frontend/src/components/merchant/CreateMenuGeneralForm.svelte
@@ -4,7 +4,7 @@
                class="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
                placeholder="" required/>
         <label for="floating_name"
-               class="peer-focus:font-medium absolute text-sm text-gray-500 400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-red-600 peer-focus:500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+               class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-red-600 peer-focus:500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
             Menu Name
         </label>
     </div>
@@ -14,7 +14,7 @@
                    class="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
                    placeholder=" " required/>
             <label for="floating_first_name"
-                   class="peer-focus:font-medium absolute text-sm text-gray-500 400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                   class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                 Date
             </label>
         </div>
@@ -23,12 +23,12 @@
                    class="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
                    placeholder=" " required/>
             <label for="floating_first_name"
-                   class="peer-focus:font-medium absolute text-sm text-gray-500 400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                   class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                 Stock
             </label>
         </div>
         <div class="flex flex-row gap-2 place-items-center mb-5">
-            <div class="flex items-center pointer-events-none mb-1">
+            <div class="flex items-center pointer-events-none">
                 Rp.
             </div>
             <div class="relative z-0 w-full group">
@@ -36,7 +36,7 @@
                        class="block py-2.5 px-0 w-full text-base text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-red-600 peer"
                        placeholder=" " required/>
                 <label for="floating_last_name"
-                       class="peer-focus:font-medium absolute text-sm text-gray-500 400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                       class="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-red-600 peer-focus:500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                     Base Price
                 </label>
             </div>
--- frontend/src/components/merchant/CreateMenuVariantForm.svelte
+++ frontend/src/components/merchant/CreateMenuVariantForm.svelte
@@ -1,3 +1,35 @@
+<script lang="ts">
+    import {toast} from "@zerodevx/svelte-toast";
+
+    interface VariantType {
+        name: string;
+        additional_price: number | null;
+    }
+
+    let variants : VariantType[] = [] ;
+
+    let addNewVariant = () => {
+        for (let i = 0; i < variants.length; i++) {
+            if (variants[i].name === '' || variants[i].additional_price === null) {
+                toast.push("Fill variant before adding new ones", {
+                    theme: {
+                        "--toastBackground": "#B02000",
+                        "--toastColor": "#fff",
+                        "--toastProgressBackground": "#fff",
+                        "--toastProgressColor": "#B02000"
+                    }
+                })
+                return;
+            }
+        }
+        variants = [...variants, {
+            name: '',
+            additional_price: null
+        }]
+    }
+
+</script>
+
 <div class="w-full mx-auto flex flex-col gap-4">
     <div class="grid md:grid-cols-2 md:gap-6">
         <div class="relative z-0 w-full group">
@@ -5,7 +37,7 @@
                 name</label>
             <input type="text" name="floating_variant_name" id="floating_variant_name"
                    class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                   value="Regular" readonly/>
+                   value="Reguler" disabled readonly/>
         </div>
 
         <div class="flex flex-row place-items-end gap-2 relative z-0 w-full group">
@@ -16,19 +48,46 @@
                 <label for="floating_additional_price"
                        class="block mb-1 text-sm font-medium text-gray-900 dark:text-white">Additional Price</label>
                 <input type="number" name="floating_additional_price" id="floating_variant_name"
-                       class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                       value="0" readonly/>
+                       class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg
+                       focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed
+                       dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400
+                       dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       value="0" disabled readonly/>
             </div>
         </div>
     </div>
-    <div class="text-red-sig flex flex-row gap-2 text-sm place-items-center">
+    {#each variants as variant}
+        <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="relative z-0 w-full group">
+                <input type="text" name="floating_variant_name" id="floating_variant_name"
+                       class="block w-full p-2.5
+                          text-sm text-gray-900 bg-transparent border-0 border-b-2
+                          border-gray-300 appearance-none focus:outline-none
+                          focus:ring-0 focus:border-red-600"
+                       value="{variant.name}"
+                       placeholder="Custom Variant (ex. Jumbo)"/>
+            </div>
+
+            <div class="flex flex-row place-items-end gap-2 relative z-0 w-full group">
+                <div class="mb-2">
+                    + Rp.
+                </div>
+                <div>
+                    <input type="number" name="floating_additional_price" id="floating_variant_name"
+                           value="{variant.additional_price}"
+                           class="block p-2.5 w-40
+                          text-sm text-gray-900 bg-transparent border-0 border-b-2
+                          border-gray-300 appearance-none focus:outline-none
+                          focus:ring-0 focus:border-red-600" placeholder="Additional Price"/>
+                </div>
+            </div>
+        </div>
+    {/each}
+    <button on:click={addNewVariant} type="button" class="text-red-sig flex flex-row gap-2 text-sm place-items-center">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
              class="w-5 h-5">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
         </svg>
-        <div>
-            Add new variant
-        </div>
-    </div>
-
+        Add new variant
+    </button>
 </div>
